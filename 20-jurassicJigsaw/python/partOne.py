648cd89bfca52fb7e89e0828b8a27343159f14f0
Tom <tom@tdpain.net>
2020-12-20 18:47:06 +0000

    Day 20, feeble part two attempt (Python)

--- 20-jurassicJigsaw/python/partOne.py
+++ 20-jurassicJigsaw/python/partOne.py
@@ -5,40 +5,9 @@ from pprint import pprint
 def partOne(instr: str) -> int:
     tiles = parse(instr)
 
-    edges = {}
-
-    # find dictionary of edges and the tiles that have them
-
-    for tile in tiles:
-        for edge in tile.edges:
-            if edge not in edges:
-                edges[edge] = set([tile.number])
-            else:
-                edges[edge].add(tile.number)
-
-    to_del = []
-
-    for edge in edges:
-        rev = "".join(reversed(edge))
-        if rev in edges and edge not in to_del:
-            edges[edge].update(edges[rev])
-            to_del.append(rev)
-
-    for r in to_del:
-        del edges[r]
-
-    # count the number of shared edges each tile has
-
-    shared_edge_count = {x.number:0 for x in tiles}
-    for tile in tiles:
-        for x in edges:
-            tn = edges[x]
-            if tile.number in tn and len(tn) > 1:
-                shared_edge_count[tile.number] += 1
+    _, shared_edge_count = get_edge_information(tiles)
 
     # find the product of all tile numbers that have 2 matching edges
-
-    pprint(shared_edge_count)
 
     c = 1
     for x in shared_edge_count:
--- 20-jurassicJigsaw/python/partTwo.py
+++ 20-jurassicJigsaw/python/partTwo.py
@@ -2,5 +2,22 @@ from common import *
 
 
 def partTwo(instr: str) -> int:
-    input_list = parse(instr)
+    tiles_list = parse(instr)
+    edges, shared_edge_count = get_edge_information(tiles_list)
+
+    # from here onwards, it's going to make more sense to use a dictionary that has tile numbers to the tile object
+    tiles = {}
+    for tile in tiles_list:
+        tiles[tile.number] = tile
+
+    used_tiles = []
+    matrix = {}
+
+    # get a single corner, set that as the start point
+    for tile_id in shared_edge_count:
+        if shared_edge_count[tile_id] == 2:
+            matrix[(0, 0)] = tiles[tile_id].components
+
+    print(matrix)
+
     return 0
