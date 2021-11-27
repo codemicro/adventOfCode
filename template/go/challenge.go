ac2f6599a5ea8663527c57556aea7331cea68934
AKP <tom@tdpain.net>
2021-11-27 14:28:52 +0000

    Add challenge template

---
+++ template/go/challenge.go
@@ -0,0 +1,15 @@
+package challenge
+
+import "github.com/codemicro/adventOfCode/lib/aocgo"
+
+type Challenge struct {
+	aocgo.BaseChallenge
+}
+
+func (c Challenge) One(instr string) (interface{}, error) {
+	return nil, nil
+}
+
+func (c Challenge) Two(instr string) (interface{}, error) {
+	return nil, nil
+}
---
+++ template/py/__init__.py
@@ -0,0 +1,13 @@
+from typing import Any
+from aocpy import BaseChallenge
+
+
+class Challenge(BaseChallenge):
+
+    @staticmethod
+    def one(instr: str) -> Any:
+        raise NotImplementedError
+
+    @staticmethod
+    def two(instr: str) -> Any:
+        raise NotImplementedError
