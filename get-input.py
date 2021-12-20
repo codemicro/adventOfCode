75d6ceb8b6af84f3fddd7e0d1f14faaf308d3adf
AKU <tom@tdpain.net>
2021-12-20 21:05:27 +0000

    Add tool to fetch inputs
    Signed-off-by: AKU <tom@tdpain.net>

---
+++ get-input.py
@@ -0,0 +1,27 @@
+import datetime
+import requests
+import json
+import os
+import sys
+import argparse
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+default_config_file_name = os.path.join(script_dir, "input-loader.json")
+
+today = datetime.date.today()
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--config-file', dest="config_file", default=default_config_file_name, help='config file name')
+parser.add_argument('--day', default=today.day, help='day to get input for')
+parser.add_argument('--year', default=today.year, help='year to get input for')
+
+args = parser.parse_args()
+
+with open(args.config_file) as f:
+    config_data = json.load(f)
+
+r = requests.get(f"https://adventofcode.com/{args.year}/day/{args.day}/input", cookies={"session": config_data["session"]})
+
+sys.stdout.write(r.text + "\n")
+
+sys.stderr.write("OK\n")
