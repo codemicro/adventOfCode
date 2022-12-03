c7f6323fba5acd7f5e0754632926eb8a7b7260ef
AKP <tom@tdpain.net>
2022-12-03 13:41:43 +0000

    Update `__init__.py`

--- template/{{cookiecutter.__formattedDayNumber}}-{{cookiecutter.__camelTitle}}/py/__init__.py
+++ template/{{cookiecutter.__formattedDayNumber}}-{{cookiecutter.__camelTitle}}/py/__init__.py
@@ -1,13 +1,12 @@
-from typing import Any
+from typing import *
 from aocpy import BaseChallenge
-
 
 class Challenge(BaseChallenge):
 
     @staticmethod
-    def one(instr: str) -> Any:
+    def one(instr: str) -> int:
         raise NotImplementedError
 
     @staticmethod
-    def two(instr: str) -> Any:
+    def two(instr: str) -> int:
         raise NotImplementedError
