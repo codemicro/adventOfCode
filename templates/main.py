1677289aeeb0c46fe56aa3ea4fe80e8ef4a80ba7
AKP <abi@tdpain.net>
2023-12-01 10:56:49 +0000

    Add Python template

---
+++ templates/main.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+
+def _debug(*args, **kwargs):
+    kwargs["file"] = sys.stderr
+    print(*args, **kwargs)
+
+
+def one(inp: str):
+    return
+
+
+def two(inp: str):
+    return
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2 or sys.argv[1] not in ["1", "2"]:
+        print("Missing day argument", file=sys.stderr)
+        os.exit(1)
+    inp = sys.stdin.read().strip()
+    if sys.argv[1] == "1":
+        print(one(inp))
+    else:
+        print(two(inp))
