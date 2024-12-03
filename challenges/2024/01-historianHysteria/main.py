ee3a3f9233f7438acfcc91be939eca84f6ff3d30
AKP <abi@tdpain.net>
2024-12-03 09:36:08 +0000

    Formatting

--- challenges/2024/01-historianHysteria/main.py
+++ challenges/2024/01-historianHysteria/main.py
@@ -4,39 +4,39 @@ from collections import defaultdict
 
 def parse(instr: str) -> tuple[list[int], list[int]]:
     a, b = [], []
-    
+
     for line in instr.splitlines():
         ai, bi = line.split("   ")
         a.append(int(ai))
         b.append(int(bi))
-        
+
     return a, b
 
 
 def one(instr: str) -> int:
     a, b = parse(instr)
-    
+
     a = sorted(a)
     b = sorted(b)
-    
+
     acc = 0
     for (x, y) in zip(a, b):
         acc += abs(y - x)
-        
+
     return acc
 
 
 def two(instr: str):
     a, b = parse(instr)
-    
+
     counts = defaultdict(lambda: 0)
     for val in b:
         counts[val] = counts[val] + 1
-    
+
     acc = 0
     for val in a:
         acc += counts[val] * val
-    
+
     return acc
 
 
